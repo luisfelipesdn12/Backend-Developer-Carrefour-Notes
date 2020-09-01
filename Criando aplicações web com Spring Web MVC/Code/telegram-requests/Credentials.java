cad44876f87623be93f392caf907017eb01c1123
Luis Felipe Santos do Nascimento <luisfelipesdn12@gmail.com>
2020-09-01 14:56:17 -0300

    Solve sec issues

--- Criando aplicações web com Spring Web MVC/Code/telegram-requests/Credentials.java
+++ Criando aplicações web com Spring Web MVC/Code/telegram-requests/Credentials.java
@@ -1,5 +1,5 @@
 public class Credentials {
-    private String botToken = "811275453:AAGxr72eYm4lmOOenUmcY8TnldZ2xSYBfjM";
+    private String botToken = "811275453:AAGxr72eYm4asfhuidavboasbsdaubsdaubsaubassbsabsaTOKEN";
 
     public String getBotToken() {
         return botToken;
